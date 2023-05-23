ad6dd01144f27343374a60ecbd9ece09af916878
Brad Wisesa <brad@grip-principle.com>
2023-05-23 15:25:12 +0700

    Update

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -3,14 +3,35 @@
 @section('container')
 <div class="row justify-content-center">
     <div class="col-md-4">
+
+        @if(session()->has('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ session('success') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
+
+        @if(session()->has('loginError'))
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                {{ session('loginError') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
+
         <main class="form-signin w-100 m-auto">
             <h1 class="h3 mb-3 fw-normal text-center">Please login</h1>
-            <form>
+            <form action="/login" method="post">
+                @csrf
                 <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="Email Address">
+                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="floatingInput" placeholder="Email Address" autofocus required value="{{ old('email') }}">
                 </div>
+                @error('email')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
                 <div class="form-floating mt-3">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password" required>
                 </div>
 
                 <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,10 +1,10 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger ">
     <div class="container">
         <a class="navbar-brand" href="/">WPU Blog</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
                     <a class="nav-link {{ ($active==='home') ? 'active' : '' }}" href="/">Home</span></a>
@@ -21,9 +21,29 @@
             </ul>
 
             <ul class="navbar-nav ms-auto">
+                @auth
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Welcome back, {{ auth()->user()->name }}
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="/dashboard">
+                            <i class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <form method="post" action="/logout">
+                            @csrf
+                            <button type="submit" class="dropdown-item">
+                                <i class="bi bi-box-arrow-right"></i> Logout
+                            </button>
+                        </form>
+                    </div>
+                </li>
+                @else
                 <li class="nav-item">
                     <a class="nav-link {{ ($active==='login') ? 'active' : '' }}" href="/login"><i class="bi bi-box-arrow-in-right"></i> Login</a>
                 </li>
+                @endauth
             </ul>
         </div>
     </div>
